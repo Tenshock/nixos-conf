cf22c4a88f97ca2766a5c149da943bb95dd48434
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-03-12 16:39:48 +0100

    feat(nix): make nix-shell use zsh

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ pkgs, config, ... }: {
   programs.zsh = {
     enable = true;
     dotDir = ".config/zsh"; # Relative path. Can't use config.xdg.configHome.
@@ -14,6 +14,18 @@
       enable = true;
       highlight = "fg=#7f849c";
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
