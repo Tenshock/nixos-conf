f7b41b574ab85a6eb0ba304ab71ebc208dad0fdc
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-03-13 10:19:17 +0100

    refactor(yazi): move to flavors

--- home/features/cli/yazi.nix
+++ home/features/cli/yazi.nix
@@ -1,15 +1,20 @@
 {pkgs, config, ... }:
 let
   mochaTheme = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/yazi/5d50620344a0c7b83f6b5b907457b835844831c3/themes/mocha/catppuccin-mocha-teal.toml";
-    sha256 = "sha256-aRnDYLzIL34KKT2U0nOaTunKLqUirQHIURW+d/grsYI=";
+    url = "https://raw.githubusercontent.com/catppuccin/yazi/5d50620344a0c7b83f6b5b907457b835844831c3/themes/mocha/catppuccin-mocha-yellow.toml";
+    sha256 = "sha256-sharychsswuyUjTUrNh8SkneMftIOX6dFE6T6Ap6lgA=";
   };
 in {
-  # TODO: switch to flavors: https://yazi-rs.github.io/docs/flavors/overview/
-  home.file."${config.xdg.configHome}/yazi/theme.toml".source = mochaTheme;
+  home.file."${config.xdg.configHome}/yazi/flavors/catppuccin-mocha.yazi/flavor.toml".source = mochaTheme;
 
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+    theme = {
+      flavor = {
+        dark = "catppuccin-mocha";
+        light = "catppuccin-mocha";
+      };
+    };
   };
 }
