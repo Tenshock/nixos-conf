010bcabbf42281278d94b72c679b80d9f9bcde2b
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-03-22 09:31:31 +0100

    fix(hibernate): update physical offset due to swap resize

--- hosts/laptop-srp/hibernate.nix
+++ hosts/laptop-srp/hibernate.nix
@@ -2,7 +2,7 @@
   boot = {
     resumeDevice = "/dev/nvme0n1p4"; #  stat <swapfile>, get Device id (number, number) && lsblk --output NAME,MAJ:MIN
     kernelParams = [
-      "resume_offset=10536960" # sudo filefrag -v <swapfile> and get first physical offset or filefrag -v /var/lib/swapfile | awk '{if(NR==4) print $4}'
+      "resume_offset=2418688" # sudo filefrag -v <swapfile> and get first physical offset or filefrag -v /var/lib/swapfile | awk '{if(NR==4) print $4}'
     ];
   };
 
