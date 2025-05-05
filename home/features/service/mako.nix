23ecc94f68a66ec0b9d0633789430c8e0f8efd0a
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-05-05 17:00:11 +0200

    chore(mako): make configuration compliant to changes

--- home/features/service/mako.nix
+++ home/features/service/mako.nix
@@ -1,41 +1,44 @@
 {
   services.mako = {
     enable  = true;
-    font    = "JetBrainsMono Nerd Font";
-    format  = ''<b>%a</b> - %s\n%b'';
-    sort    = "-time";
+    settings = {
+      font    = "JetBrainsMono Nerd Font";
+      format  = ''<b>%a</b> - %s\n%b'';
+      sort    = "-time";
 
-    ### Layout
-    layer   = "overlay";
-    width   = 300;
-    height  = 110;
-    margin = "5,0,0,0";
-    padding = "0,5,10,10";
-    borderSize = 2;
-    borderRadius = 5;
-    icons = true;
+      ### Layout
+      layer   = "overlay";
+      width   = 300;
+      height  = 110;
+      margin = "5,0,0,0";
+      padding = "0,5,10,10";
+      borderSize = 2;
+      borderRadius = 5;
+      icons = true;
 
-    ### Behavior
-    defaultTimeout = 10000;
-    ignoreTimeout = true;
+      ### Behavior
+      defaultTimeout = 10000;
+      ignoreTimeout = true;
 
-    ### Colors
-    backgroundColor = "#181825f0";
-    textColor = "#cdd6f4";
-    progressColor = "#313244";
+      ### Colors
+      backgroundColor = "#181825f0";
+      textColor = "#cdd6f4";
+      progressColor = "#313244";
 
-    extraConfig = ''
-      outer-margin=10,15,0,0
+      outer-margin = "10,15,0,0";
 
-      [urgency=low]
-      border-color=#8bd5ca
+      "[urgency=low]" = {
+        border-color = "#8bd5ca";
+      };
 
-      [urgency=normal]
-      border-color=#8aadf4
+      "[urgency=normal]" = {
+        border-color = "#8aadf4";
+      };
 
-      [urgency=critical]
-      border-color=#f38ba8
-      default-timeout=0
-    '';
+      "[urgency=critical]" = {
+        border-color = "#f38ba8";
+        default-timeout = 0;
+      };
+    };
   };
 }
