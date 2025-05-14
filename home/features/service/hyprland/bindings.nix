08cf73f10b2544f1217748c4a34dac3e5c75abae
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-05-14 15:21:37 +0200

    feat(hyprland): add play-pause binding on F9

--- home/features/service/hyprland/bindings.nix
+++ home/features/service/hyprland/bindings.nix
@@ -72,6 +72,7 @@
     bindl = [
       ", XF86AudioNext, exec, playerctl next"
       ", XF86AudioPause, exec, playerctl play-pause"
+      ", F9, exec, playerctl play-pause"
       ", XF86AudioPlay, exec, playerctl play-pause"
       ", XF86AudioPrev, exec, playerctl previous"
     ];
