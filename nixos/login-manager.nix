9e5e1375d36ff0509cbe0a0210127a4d9315484a
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-06-05 08:24:16 +0200

    feat(greetd): remember last session

--- nixos/login-manager.nix
+++ nixos/login-manager.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --theme 'text=darkgray;time=darkgray;container=darkgray;border=gray;title=darkgray;greet=darkgray;prompt=white;input=white;action=lightcyan;button=cyan' --greeting 'Authenticate to the sleeper build'";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --theme 'text=darkgray;time=darkgray;container=darkgray;border=gray;title=darkgray;greet=darkgray;prompt=white;input=white;action=lightcyan;button=cyan' --greeting 'Authenticate to the sleeper build' --remember-session";
         user = "greeter";
       };
     };
