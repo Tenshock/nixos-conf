1a3592a07c75de0c79c38a84c30709da1abcd709
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-10-22 10:48:00 +0200

    feat(rust): install and update toolchain

--- home/features/prog-lang/rust.nix
+++ home/features/prog-lang/rust.nix
@@ -1,1 +1,11 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ gcc rustup ]; }
+{ pkgs, lib, ... }: {
+  home = {
+    packages = with pkgs; [ gcc rustup ];
+
+    activation.updateRust = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${pkgs.rustup}/bin/rustup -q install stable
+      ${pkgs.rustup}/bin/rustup -q update
+      ${pkgs.rustup}/bin/rustup -q default stable
+    '';
+  };
+}
