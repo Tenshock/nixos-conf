f20967d1636434affcf0b7cf6649238a345d9cf0
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-11-25 11:55:46 +0100

    chore: clean ssh configuration

--- home/features/service/ssh.nix
+++ home/features/service/ssh.nix
@@ -9,16 +9,6 @@
           ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''
         ];
       };
-
-      "github.com" = {
-        hostname = "ssh.github.com";
-        port = 443;
-      };
-
-      "gitlab.com" = {
-        hostname = "altssh.gitlab.com";
-        port = 443;
-      };
     };
   };
 }
