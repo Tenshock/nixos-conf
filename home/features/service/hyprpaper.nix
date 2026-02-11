489ff5359059feaa63fac364d94e08a672cc1d70
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-02-11 22:13:27 +0100

    feat(hyprpaper): disable splash messages

--- home/features/service/hyprpaper.nix
+++ home/features/service/hyprpaper.nix
@@ -7,6 +7,7 @@ in {
   services.hyprpaper = {
     enable = true;
     settings = {
+      splash = false;
       preload = [ "${wallpaperDestDir}/${wallpaper}" ];
       wallpaper = [{
         monitor = "";
