9cdfa9f9c30c4361c45d34dfa9af14ae38af57dd
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-02-14 19:59:02 +0100

    feat: replace discord by vesktop client

--- home/features/application/discord.nix
+++ home/features/application/discord.nix
@@ -1,1 +1,6 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ discord ]; }
+{
+  programs.vesktop = {
+    enable = true;
+    vencord.settings = { plugins = { FakeNitro.enabled = true; }; };
+  };
+}
