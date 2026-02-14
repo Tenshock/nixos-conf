065dd4403886f4d82999b72c98326370625112e2
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-02-14 21:13:08 +0100

    feat: scope tuigreet to uwsm hyprland session

--- nixos/login-manager.nix
+++ nixos/login-manager.nix
@@ -4,11 +4,14 @@
     settings = {
       default_session = {
         command =
-          "${pkgs.tuigreet}/bin/tuigreet --time --theme 'text=darkgray;time=darkgray;container=darkgray;border=gray;title=darkgray;greet=darkgray;prompt=white;input=white;action=lightcyan;button=cyan' --greeting 'Authenticate to the sleeper build' --remember-session";
+          "${pkgs.tuigreet}/bin/tuigreet --time --sessions /etc/greetd/sessions --theme 'text=darkgray;time=darkgray;container=darkgray;border=gray;title=darkgray;greet=darkgray;prompt=white;input=white;action=lightcyan;button=cyan' --greeting 'Authenticate to the sleeper build' --remember-session";
         user = "greeter";
       };
     };
   };
+
+  environment.etc."greetd/sessions/hyprland-uwsm.desktop".source =
+    "${pkgs.hyprland}/share/wayland-sessions/hyprland-uwsm.desktop";
 
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
