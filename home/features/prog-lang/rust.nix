26163ddc3c8eaf8d67ba401a6c3b57ce0f516ef0
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-02-14 23:48:00 +0100

    feat: disable auto-rust home-manager update

--- home/features/prog-lang/rust.nix
+++ home/features/prog-lang/rust.nix
@@ -1,11 +1,11 @@
-{ pkgs, lib, ... }: {
+{ pkgs, ... }: {
   home = {
     packages = with pkgs; [ gcc rustup ];
 
-    activation.updateRust = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ${pkgs.rustup}/bin/rustup -q install stable
-      ${pkgs.rustup}/bin/rustup -q update
-      ${pkgs.rustup}/bin/rustup -q default stable
-    '';
+    # activation.updateRust = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    #   ${pkgs.rustup}/bin/rustup -q install stable
+    #   ${pkgs.rustup}/bin/rustup -q update
+    #   ${pkgs.rustup}/bin/rustup -q default stable
+    # '';
   };
 }
