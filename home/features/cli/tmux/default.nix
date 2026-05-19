977b9eaaf19c6483ea8951f15e13055be3671b26
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-05-20 00:36:09 +0200

    feat: move back tmux config

--- home/features/cli/tmux/default.nix
+++ home/features/cli/tmux/default.nix
@@ -47,14 +47,14 @@
       # set-hook -g session-window-changed 'run-shell "tmux-track-and-clean"'
 
       # AZERTY Alt+number window switch
-      bind-key -n M-1  run-shell 'tmux-goto-window 1'
+      bind-key -n M-&  run-shell 'tmux-goto-window 1'
       bind-key -n M-2  run-shell 'tmux-goto-window 2'
-      bind-key -n M-3 run-shell 'tmux-goto-window 3'
-      bind-key -n M-4 run-shell 'tmux-goto-window 4'
-      bind-key -n M-5 run-shell 'tmux-goto-window 5'
-      bind-key -n M-6  run-shell 'tmux-goto-window 6'
+      bind-key -n M-\" run-shell 'tmux-goto-window 3'
+      bind-key -n M-\' run-shell 'tmux-goto-window 4'
+      bind-key -n M-\( run-shell 'tmux-goto-window 5'
+      bind-key -n M--  run-shell 'tmux-goto-window 6'
       bind-key -n M-7  run-shell 'tmux-goto-window 7'
-      bind-key -n M-8  run-shell 'tmux-goto-window 8'
+      bind-key -n M-_  run-shell 'tmux-goto-window 8'
       bind-key -n M-9  run-shell 'tmux-goto-window 9'
       bind-key -n M-0  run-shell 'tmux-goto-window 10'
 
