25bb9f8065d65f7fc62efcc0b843052d5ed77137
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-23 22:51:12 +0200

    feat(zen): set catppuccin theme

--- home/features/application/zen.nix
+++ home/features/application/zen.nix
@@ -7,13 +7,32 @@ let
   inherit (inputs.firefox-addons.overlays.default pkgs pkgs) firefox-addons;
 in
 {
+  home.file.".zen/e3dki2qs.Default Profile/chrome/userChrome.css".source =
+    ./zen/catppuccin-mocha-peach/userChrome.css;
+  home.file.".zen/e3dki2qs.Default Profile/chrome/userContent.css".source =
+    ./zen/catppuccin-mocha-peach/userContent.css;
+  home.file.".zen/e3dki2qs.Default Profile/chrome/zen-logo-mocha.svg".source =
+    ./zen/catppuccin-mocha-peach/zen-logo-mocha.svg;
+
+  xdg.configFile."zen/default/chrome/zen-logo-mocha.svg".source =
+    ./zen/catppuccin-mocha-peach/zen-logo-mocha.svg;
+
   programs.zen-browser = {
     enable = true;
+    extraPrefs = ''
+      pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+    '';
     setAsDefaultBrowser = true;
 
-    profiles.default.extensions.packages = with firefox-addons; [
-      ublock-origin
-      onepassword-password-manager
-    ];
+    profiles.default = {
+      extensions.packages = with firefox-addons; [
+        ublock-origin
+        onepassword-password-manager
+      ];
+
+      settings."toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      userChrome = ./zen/catppuccin-mocha-peach/userChrome.css;
+      userContent = ./zen/catppuccin-mocha-peach/userContent.css;
+    };
   };
 }
