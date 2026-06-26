61c19f0ef2e2f10e7c919015fba11795838c8e99
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-26 23:31:21 +0200

    feat(steam): add proton GE compatibility layer

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -2,6 +2,7 @@
 {
   programs.steam = {
     enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
