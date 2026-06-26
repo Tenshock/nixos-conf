922bc4617e06e2aa642c56c266fc541a5ae8cb22
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-27 01:14:37 +0200

    feat(transmission): prevent laptop to sleep when transmission running

--- home/features/application/transmission.nix
+++ home/features/application/transmission.nix
@@ -3,13 +3,21 @@ let
   transmissionWayland = pkgs.symlinkJoin {
     name = "transmission-wayland";
     paths = [ pkgs.transmission_4-qt ];
-    nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       rm $out/bin/transmission-qt
-      makeWrapper ${pkgs.transmission_4-qt}/bin/transmission-qt $out/bin/transmission-qt \
-        --unset DISPLAY \
-        --set QT_QPA_PLATFORM "wayland" \
-        --prefix QT_PLUGIN_PATH : "${pkgs.qt5.qtwayland.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+      cat > $out/bin/transmission-qt <<'EOF'
+      #!${pkgs.runtimeShell}
+      unset DISPLAY
+      export QT_QPA_PLATFORM=wayland
+      export QT_PLUGIN_PATH="${pkgs.qt5.qtwayland.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}:''${QT_PLUGIN_PATH:-}"
+
+      exec ${pkgs.systemd}/bin/systemd-inhibit \
+        --what=sleep:handle-lid-switch \
+        --who=Transmission \
+        --why="Transmission is running" \
+        ${pkgs.transmission_4-qt}/bin/transmission-qt "$@"
+      EOF
+      chmod +x $out/bin/transmission-qt
 
       rm $out/share/applications/transmission-qt.desktop
       substitute ${pkgs.transmission_4-qt}/share/applications/transmission-qt.desktop $out/share/applications/transmission-qt.desktop \
