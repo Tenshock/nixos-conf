ca833a6e57dfc11c065749db9252b5b8f1a2366f
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-26 15:27:52 +0200

    feat(nixos): update default apps for video and images

--- home/features/service/xdg.nix
+++ home/features/service/xdg.nix
@@ -4,6 +4,76 @@
   config,
   ...
 }:
+let
+  browser = "zen.desktop";
+  imageViewer = "org.gnome.Loupe.desktop";
+  videoPlayer = "vlc.desktop";
+
+  defaultsFor = desktopFile: mimeTypes: lib.genAttrs mimeTypes (_: [ desktopFile ]);
+
+  webMimeTypes = [
+    "x-scheme-handler/http"
+    "x-scheme-handler/https"
+    "text/html"
+    "application/xhtml+xml"
+  ];
+
+  pdfMimeTypes = [
+    "application/pdf"
+  ];
+
+  videoMimeTypes = [
+    "video/x-matroska" # .mkv
+    "video/mp4" # .mp4, .m4v
+    "video/x-m4v" # .m4v
+    "video/webm" # .webm
+    "video/x-msvideo" # .avi
+    "video/quicktime" # .mov
+    "video/x-ms-wmv" # .wmv
+    "video/mpeg" # .mpeg, .mpg
+    "video/ogg" # .ogv
+    "video/x-flv" # .flv
+    "video/3gpp" # .3gp
+    "video/3gpp2" # .3g2
+    "video/mp2t" # .ts
+    "application/vnd.rn-realmedia" # .rm
+  ];
+
+  imageMimeTypes = [
+    "image/apng"
+    "image/bmp"
+    "image/gif"
+    "image/jp2"
+    "image/jpeg"
+    "image/png"
+    "image/qoi"
+    "image/tiff"
+    "image/vnd.microsoft.icon"
+    "image/webp"
+    "image/x-dds"
+    "image/x-exr"
+    "image/x-portable-anymap"
+    "image/x-portable-bitmap"
+    "image/x-portable-graymap"
+    "image/x-portable-pixmap"
+    "image/x-qoi"
+    "image/x-tga"
+    "image/x-win-bitmap"
+    "image/x-xbitmap"
+    "image/x-xpixmap"
+    "image/svg+xml"
+    "image/svg+xml-compressed"
+    "image/avif"
+    "image/heic"
+    "image/jxl"
+  ];
+
+  defaultApplications =
+    defaultsFor browser webMimeTypes
+    // defaultsFor browser pdfMimeTypes
+    // defaultsFor videoPlayer videoMimeTypes
+    // defaultsFor imageViewer imageMimeTypes;
+in
 {
   xdg = {
     enable = true;
@@ -15,20 +85,8 @@
 
     mimeApps = lib.mkIf (!pkgs.stdenv.isDarwin) {
       enable = true;
-      defaultApplications = {
-        # URLs
-        "x-scheme-handler/http" = [ "zen.desktop" ];
-        "x-scheme-handler/https" = [ "zen.desktop" ];
-        "text/html" = [ "zen.desktop" ];
-        "application/xhtml+xml" = [ "zen.desktop" ];
-
-        # PDF
-        "application/pdf" = [ "zen.desktop" ];
-
-        # Videos
-        "video/*" = [ "vlc.desktop" ];
-        "application/vnd.rn-realmedia" = [ "vlc.desktop" ]; # .rm
-      };
+      associations.added = defaultApplications;
+      inherit defaultApplications;
     };
   };
 }
