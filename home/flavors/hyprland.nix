ecd9967b844a77de20f7092fe9da7e3a57fee6dc
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-27 19:01:18 +0200

    refactor(nixos): remove unused hyprpaper dependency

--- home/flavors/hyprland.nix
+++ home/flavors/hyprland.nix
@@ -13,7 +13,6 @@
     ../features/service/hypridle.nix
     ../features/service/hyprland
     ../features/service/hyprlock.nix
-    ../features/service/hyprpaper.nix
     ../features/service/mako.nix
     ../features/service/power-menu.nix
     ../features/service/waycorner.nix
