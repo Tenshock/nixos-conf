5f95f7537aad7f5695bf3f4993fbe2fb3da45216
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-27 21:05:27 +0200

    refactor(hyprland): move app declarations into own file

---
+++ home/features/service/hyprland/apps.nix
@@ -0,0 +1,19 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    audioManager._var = "uwsm app -- $(kitty -e ncpamixer)";
+    bluetoothManager._var = "uwsm app -- kitty -e bluetui";
+    browser._var = "uwsm app -- zen-twilight";
+    fileManager._var = "uwsm app -- thunar";
+    hyprlock._var = "uwsm app -- hyprlock";
+    networkManager._var = "uwsm app -- $(kitty -e nmtui)";
+    notificationCenter._var = "uwsm app -- swaync-client -t -sw";
+    obsidian._var = "uwsm app -- obsidian";
+    powerMenu._var = "uwsm app -- power-menu";
+    smile._var = "uwsm app -- smile";
+    statusbar._var = "uwsm app -- waybar";
+    teams._var = "uwsm app -- teams-for-linux";
+    terminal._var = "uwsm app -- kitty";
+    vesktop._var = "uwsm app -- vesktop";
+    walker._var = "uwsm app -- walker";
+  };
+}
--- home/features/service/hyprland/default.nix
+++ home/features/service/hyprland/default.nix
@@ -9,6 +9,7 @@
   services.hyprpolkitagent.enable = true;
 
   imports = [
+    ./apps.nix
     ./bindings.nix
     ./ui.nix
   ];
@@ -19,23 +20,6 @@
     systemd.enable = false; # Necessary for UWSM integration. See nixos/hyprland.nix
 
     settings = {
-      # Apps
-      audioManager._var = "uwsm app -- $(kitty -e ncpamixer)";
-      bluetoothManager._var = "uwsm app -- kitty -e bluetui";
-      browser._var = "uwsm app -- zen-twilight";
-      fileManager._var = "uwsm app -- thunar";
-      hyprlock._var = "uwsm app -- hyprlock";
-      networkManager._var = "uwsm app -- $(kitty -e nmtui)";
-      notificationCenter._var = "uwsm app -- swaync-client -t -sw";
-      obsidian._var = "uwsm app -- obsidian";
-      powerMenu._var = "uwsm app -- power-menu";
-      smile._var = "uwsm app -- smile";
-      statusbar._var = "uwsm app -- waybar";
-      teams._var = "uwsm app -- teams-for-linux";
-      terminal._var = "uwsm app -- kitty";
-      vesktop._var = "uwsm app -- vesktop";
-      walker._var = "uwsm app -- walker";
-
       gesture = [
         {
           fingers = 3;
