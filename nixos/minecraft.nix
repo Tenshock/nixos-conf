6bd57b4cb0b5c6c2c281e5c1788ac1961842c1ba
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-27 19:16:12 +0200

    refactor(nixos): remove minecraft server config

--- nixos/minecraft.nix
+++ nixos/minecraft.nix
@@ -6,15 +6,4 @@ user:
       prismlauncher
     ];
   };
-
-  services.minecraft-server = {
-    enable = false;
-    eula = true;
-    openFirewall = true;
-    declarative = true;
-    serverProperties = {
-      motd = "NixOS Minecraft server!";
-    };
-    jvmOpts = "-Xms6G -Xmx6G -XX:+UseZGC";
-  };
 }
