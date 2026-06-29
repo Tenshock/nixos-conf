2a8a6e7a026609482e3ca417e42f38ecb93eaef7
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-29 10:34:54 +0200

    feat(steam): add lsfg-vk layer

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    lsfg-vk
+    lsfg-vk-ui
+  ];
+
   programs.steam = {
     enable = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
