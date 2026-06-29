a59854ef2cf027c2263c68aa58879933a95ecb3d
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-29 10:34:37 +0200

    feat(discord): update tray icon

--- home/features/application/discord.nix
+++ home/features/application/discord.nix
@@ -1,4 +1,10 @@
 {
+  xdg.configFile = {
+    "vesktop/userAssets/tray".source = ../service/waybar/style/icons/vesktop-brand-discord-red.png;
+    "vesktop/userAssets/trayUnread".source =
+      ../service/waybar/style/icons/vesktop-brand-discord-red.png;
+  };
+
   programs.vesktop = {
     enable = true;
     vencord.settings = {
