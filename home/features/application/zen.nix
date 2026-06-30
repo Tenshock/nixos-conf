7a28c356942a0c061664e3cbff1d83bfea20a27a
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-06-30 16:58:01 +0200

    refactor(zen): remove custom styling

--- home/features/application/zen.nix
+++ home/features/application/zen.nix
@@ -7,25 +7,8 @@ let
   inherit (inputs.firefox-addons.overlays.default pkgs pkgs) firefox-addons;
 in
 {
-  home = {
-    file = {
-      ".zen/e3dki2qs.Default Profile/chrome/userChrome.css".source =
-        ./zen/catppuccin-mocha-peach/userChrome.css;
-      ".zen/e3dki2qs.Default Profile/chrome/userContent.css".source =
-        ./zen/catppuccin-mocha-peach/userContent.css;
-      ".zen/e3dki2qs.Default Profile/chrome/zen-logo-mocha.svg".source =
-        ./zen/catppuccin-mocha-peach/zen-logo-mocha.svg;
-    };
-  };
-
-  xdg.configFile."zen/default/chrome/zen-logo-mocha.svg".source =
-    ./zen/catppuccin-mocha-peach/zen-logo-mocha.svg;
-
   programs.zen-browser = {
     enable = true;
-    extraPrefs = ''
-      pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-    '';
     setAsDefaultBrowser = true;
 
     profiles.default = {
@@ -34,9 +17,6 @@ in
         onepassword-password-manager
       ];
 
-      settings."toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      userChrome = ./zen/catppuccin-mocha-peach/userChrome.css;
-      userContent = ./zen/catppuccin-mocha-peach/userContent.css;
     };
   };
 }
