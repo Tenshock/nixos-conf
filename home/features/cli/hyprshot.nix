b6916971408235ce324f746699bc6fff30e84cf2
Cedric Prezelin <cedric.prezelin@gmail.com>
2026-07-03 00:15:44 +0200

    feat(hyprshot): open on default app on notification click

--- home/features/cli/hyprshot.nix
+++ home/features/cli/hyprshot.nix
@@ -1,6 +1,34 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+let
+  hyprshot-xdg-open = pkgs.hyprshot.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.perl ];
+
+    postPatch = (old.postPatch or "") + ''
+      perl -0pi -e 's!notify-send "Screenshot saved" \\\n\s+"\$\{message\}" \\\n\s+-t "\$NOTIF_TIMEOUT" -i "\$\{1\}" -a Hyprshot!q~(
+        local action
+        action=$(
+            notify-send "Screenshot saved" \
+                        "''${message}" \
+                        -t "$NOTIF_TIMEOUT" -i "''${1}" -a Hyprshot \
+                        --action default=Open --wait
+        )
+        case "$action" in
+            default)
+                xdg-open "$1" >/dev/null 2>&1 &
+                ;;
+        esac
+    ) &~!se' hyprshot
+    '';
+
+    postFixup = (old.postFixup or "") + ''
+      wrapProgram "$out/bin/hyprshot" \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.xdg-utils ]}
+    '';
+  });
+in
+{
   home = {
-    packages = with pkgs; [ hyprshot ];
+    packages = [ hyprshot-xdg-open ];
 
     sessionVariables = {
       XDG_PICTURES_DIR = "${config.home.homeDirectory}/pictures";
