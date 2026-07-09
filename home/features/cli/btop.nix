b7fd3cd7c097c7e14f80343c11726a33874b6d08
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-07-09 20:05:26 +0200

    feat(btop): update configuration

--- home/features/cli/btop.nix
+++ home/features/cli/btop.nix
@@ -1,4 +1,21 @@
+{ lib, pkgs, ... }:
 {
-  programs.btop.enable = true;
+  programs.btop = {
+    enable = true;
+    package =
+      if pkgs.stdenv.hostPlatform.isLinux then
+        pkgs.btop.override {
+          cudaSupport = true;
+          rocmSupport = true;
+        }
+      else
+        pkgs.btop;
+    settings = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+      shown_boxes = "cpu mem net proc gpu0 gpu1";
+      shown_gpus = "nvidia amd";
+      custom_gpu_name0 = "RTX 5070";
+      custom_gpu_name1 = "Radeon 890M";
+    };
+  };
   catppuccin.btop.enable = true;
 }
