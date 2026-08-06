7eb752ad7c9a735a9730b8ba8fb61e0b68e05fd8
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-08-06 23:43:02 +0200

    fix(chatgpt-desktop): remove stale option

--- nixos/chatgpt-desktop.nix
+++ nixos/chatgpt-desktop.nix
@@ -3,12 +3,5 @@
 {
   imports = [ inputs.chatgpt-desktop-linux.nixosModules.default ];
 
-  programs.chatgptDesktop = {
-    enable = true;
-    githubTokenCommand = [
-      "/run/wrappers/bin/op"
-      "read"
-      "op://Personal/github.com/NixOS PAT"
-    ];
-  };
+  programs.chatgptDesktop.enable = true;
 }
