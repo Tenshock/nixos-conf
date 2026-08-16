f6113d64ef97e9f2e45c27adcd12594424dfb2c9
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-08-16 19:29:58 +0200

    feat(nixos): disable fingerprint auth

--- nixos/hardware/fingerprint.nix
+++ nixos/hardware/fingerprint.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 {
   security.pam.services = {
-    login.fprintAuth = true;
+    login.fprintAuth = false;
+    greetd.fprintAuth = false;
+
     sudo.fprintAuth = true;
     hyprlock.fprintAuth = true;
+    polkit-agent-helper-1.fprintAuth = true;
+
     hyprlock.text = ''
       auth sufficient ${pkgs.pam}/lib/security/pam_unix.so try_first_pass
       auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
       auth required ${pkgs.pam}/lib/security/pam_deny.so
     '';
-    greetd.fprintAuth = true;
-    polkit-agent-helper-1.fprintAuth = true;
   };
 
   services.fprintd = {
